cb4ff1652d4d931c1955c9ef3b0c4ed0ec81c0bf
harsh23exe <harsh23exe@gmail.com>
2023-07-12 14:36:25 +0530

    Setting up springboot

---
+++ src/main/java/com/Spring_project/Keycloackuser/KeycloackuserApplication.java
@@ -0,0 +1,18 @@
+package com.Spring_project.Keycloackuser;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
+
+@SpringBootApplication(exclude = {SecurityAutoConfiguration.class})
+public class KeycloackuserApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KeycloackuserApplication.class, args);
+	}
+
+}
+
+
+
+
---
+++ src/test/java/com/Spring_project/Keycloackuser/KeycloackuserApplicationTests.java
@@ -0,0 +1,13 @@
+package com.Spring_project.Keycloackuser;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KeycloackuserApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
